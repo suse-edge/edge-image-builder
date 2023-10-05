a402e0ecb83c3857eaba914d2697d4c2643f8d8f
Jason Dobies <jason_dobies@yahoo.com>
2023-10-05 15:09:52 -0400

    Added initial CLI arguments (#3)
    * Added first round of CLI arguments
    
    * simplified by removing FlagSet
    
    * incorporated PR feedback
    
    * delegated the file existence checks to ReadFile

--- cmd/eib/main.go
+++ cmd/eib/main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/suse-edge/edge-image-builder/pkg/config"
+)
+
+const (
+	argConfigFile = "config"
+	argVerbose    = "verbose"
+)
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp:    true,
+		QuoteEmptyFields: true,
+	})
+	log.SetOutput(os.Stdout)
+}
+
+func processArgs() (*config.ImageConfig, error) {
+	var (
+		configFile string
+		verbose    bool
+	)
+
+	flag.StringVar(&configFile, argConfigFile, "", "full path to the image configuration file")
+	flag.BoolVar(&verbose, argVerbose, false, "enables extra logging information")
+	flag.Parse()
+
+	handleVerbose(verbose)
+	imageConfig, err := handleImageConfig(configFile)
+
+	return imageConfig, err
+}
+
+func handleVerbose(verbose bool) {
+	if verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+}
+
+func handleImageConfig(configFile string) (*config.ImageConfig, error) {
+	if configFile == "" {
+		return nil, fmt.Errorf("the \"%s\" argument must be specified", argConfigFile)
+	}
+
+	configData, err := os.ReadFile(configFile)
+	if err != nil {
+		return nil, fmt.Errorf("image configuration file \"%s\" cannot be read: %s", configFile, err)
+	}
+
+	imageConfig, err := config.Parse(configData)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing configuration file \"%s\": %s", configFile, err)
+	}
+
+	return imageConfig, nil
+}
 
 func main() {
-	fmt.Println("Edge Image Builder")
+	_, err := processArgs()
+	if err != nil {
+		log.Error(err)
+	}
+
+	// Call to building logic when it's finished
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/suse-edge/edge-image-builder
 go 1.21.1
 
 require (
+	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.8.4
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -10,4 +11,5 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
