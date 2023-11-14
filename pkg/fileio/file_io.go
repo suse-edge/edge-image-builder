b56c99376a45ed18e81263b1d63b7db56b93eb48
Atanas Dinov <atanas.dinov@suse.com>
2023-11-14 12:46:54 +0200

    Restructure file writing
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>

--- pkg/fileio/file_io.go
+++ pkg/fileio/file_io.go
@@ -7,27 +7,30 @@ import (
 )
 
 func WriteFile(filename string, contents string, templateData any) error {
-	if templateData != nil {
-		tmpl, err := template.New(filename).Parse(contents)
-		if err != nil {
-			return fmt.Errorf("creating template for file %s: %w", filename, err)
+	if templateData == nil {
+		if err := os.WriteFile(filename, []byte(contents), os.ModePerm); err != nil {
+			return fmt.Errorf("writing file: %w", err)
 		}
 
-		file, err := os.Create(filename)
-		if err != nil {
-			return fmt.Errorf("creating file %s: %w", filename, err)
-		}
-		defer file.Close()
-
-		err = tmpl.Execute(file, templateData)
-		if err != nil {
-			return fmt.Errorf("applying the template at %s: %w", filename, err)
-		}
-	} else {
-		err := os.WriteFile(filename, []byte(contents), os.ModePerm)
-		if err != nil {
-			return fmt.Errorf("writing file %s: %w", filename, err)
-		}
+		return nil
 	}
+
+	tmpl, err := template.New(filename).Parse(contents)
+	if err != nil {
+		return fmt.Errorf("parsing template: %w", err)
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("creating file: %w", err)
+	}
+	defer func() {
+		_ = file.Close()
+	}()
+
+	if err = tmpl.Execute(file, templateData); err != nil {
+		return fmt.Errorf("applying template: %w", err)
+	}
+
 	return nil
 }
