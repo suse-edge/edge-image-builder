ce82300e095b2c710e83ddd380c83561ed6700c5
Atanas Dinov <atanas.dinov@suse.com>
2023-11-14 12:47:10 +0200

    Test file writing
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>

--- pkg/fileio/file_io_test.go
+++ pkg/fileio/file_io_test.go
@@ -9,29 +9,74 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestWriteFileWithTemplate(t *testing.T) {
-	// Setup
-	tmpDir, err := os.MkdirTemp("", "eib-test-")
-	require.NoError(t, err)
-	defer os.RemoveAll(tmpDir)
-
-	testData := "{{.Foo}} and {{.Bar}}"
-	values := struct {
-		Foo string
-		Bar string
+func TestWriteFile(t *testing.T) {
+	tests := []struct {
+		name             string
+		filename         string
+		contents         string
+		templateData     any
+		expectedContents string
+		expectedErr      string
 	}{
-		Foo: "ooF",
-		Bar: "raB",
+		{
+			name:             "Standard file is successfully written",
+			filename:         "standard",
+			contents:         "this is a non-templated file",
+			expectedContents: "this is a non-templated file",
+		},
+		{
+			name:     "Templated file is successfully written",
+			filename: "template",
+			contents: "{{.Foo}} and {{.Bar}}",
+			templateData: struct {
+				Foo string
+				Bar string
+			}{
+				Foo: "ooF",
+				Bar: "raB",
+			},
+			expectedContents: "ooF and raB",
+		},
+		{
+			name:         "Templated file is not written due to invalid syntax",
+			filename:     "invalid-syntax",
+			contents:     "{{.Foo and ",
+			templateData: struct{}{},
+			expectedErr:  "parsing template: template: write-test/invalid-syntax:1: unclosed action",
+		},
+		{
+			name:     "Templated file is not written due to missing field",
+			filename: "invalid-data",
+			contents: "{{.Foo}} and {{.Bar}}",
+			templateData: struct {
+				Foo string
+			}{
+				Foo: "ooF",
+			},
+			expectedErr: "applying template: template: write-test/invalid-data:1:15: " +
+				"executing \"write-test/invalid-data\" at <.Bar>: can't evaluate field Bar in type struct { Foo string }",
+		},
 	}
-	testFilename := filepath.Join(tmpDir, "write-file-with-template.sh")
 
-	// Test
-	err = WriteFile(testFilename, testData, &values)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			tmpDir := "write-test"
+			require.NoError(t, os.Mkdir(tmpDir, os.ModePerm))
+			defer os.RemoveAll(tmpDir)
 
-	// Verify
-	require.NoError(t, err)
+			filename := filepath.Join(tmpDir, test.filename)
+			err := WriteFile(filename, test.contents, test.templateData)
+			if test.expectedErr != "" {
+				assert.EqualError(t, err, test.expectedErr)
+				return
+			}
 
-	foundData, err := os.ReadFile(testFilename)
-	require.NoError(t, err)
-	assert.Equal(t, "ooF and raB", string(foundData))
+			require.Nil(t, err)
+
+			contents, err := os.ReadFile(filename)
+			require.NoError(t, err)
+
+			assert.Equal(t, test.expectedContents, string(contents))
+		})
+	}
 }
