287fba3676fadcf28fa049376f6681851c505563
Atanas Dinov <atanas.dinov@suse.com>
2023-11-15 10:36:59 +0200

    Add DirStructure type
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>

---
+++ pkg/build/dir_structure.go
@@ -0,0 +1,50 @@
+package build
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type DirStructure struct {
+	// ImageConfigDir is the root directory storing all configuration files.
+	ImageConfigDir string
+	// BuildDir is the directory used for assembling the different components used in a build.
+	BuildDir string
+	// CombustionDir is a subdirectory under BuildDir containing the Combustion script and all related files.
+	CombustionDir string
+	// DeleteBuildDir indicates whether the BuildDir should be cleaned up after the image is built.
+	DeleteBuildDir bool
+}
+
+func NewDirStructure(imageConfigDir, buildDir string, deleteBuildDir bool) (*DirStructure, error) {
+	if buildDir == "" {
+		tmpDir, err := os.MkdirTemp("", "eib-")
+		if err != nil {
+			return nil, fmt.Errorf("creating a temporary build directory: %w", err)
+		}
+		buildDir = tmpDir
+	}
+	combustionDir := filepath.Join(buildDir, "combustion")
+
+	if err := os.MkdirAll(combustionDir, os.ModePerm); err != nil {
+		return nil, fmt.Errorf("creating the combustion directory: %w", err)
+	}
+
+	return &DirStructure{
+		ImageConfigDir: imageConfigDir,
+		BuildDir:       buildDir,
+		CombustionDir:  combustionDir,
+		DeleteBuildDir: deleteBuildDir,
+	}, nil
+}
+
+func (ds *DirStructure) cleanUpBuildDir() error {
+	if ds.DeleteBuildDir {
+		err := os.RemoveAll(ds.BuildDir)
+		if err != nil {
+			return fmt.Errorf("deleting build directory: %w", err)
+		}
+	}
+	return nil
+}
---
+++ pkg/build/dir_structure_test.go
@@ -0,0 +1,71 @@
+package build
+
+import (
+	"io/fs"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDirStructure_New(t *testing.T) {
+	ds, err := NewDirStructure("", "", false)
+	require.NoError(t, err)
+	defer os.RemoveAll(ds.BuildDir)
+
+	_, err = os.Stat(ds.BuildDir)
+	require.NoError(t, err)
+}
+
+func TestDirStructure_New_ExistingBuildDir(t *testing.T) {
+	// Setup
+	tmpDir, err := os.MkdirTemp("", "eib-test-")
+	require.NoError(t, err)
+	defer os.RemoveAll(tmpDir)
+
+	// Test
+	ds, err := NewDirStructure("", tmpDir, false)
+	require.NoError(t, err)
+
+	// Verify
+	require.Equal(t, tmpDir, ds.BuildDir)
+}
+
+func TestDirStructure_CleanUpBuildDirTrue(t *testing.T) {
+	// Setup
+	tmpDir, err := os.MkdirTemp("", "eib-test-")
+	require.NoError(t, err)
+	defer os.RemoveAll(tmpDir)
+
+	ds := DirStructure{
+		BuildDir:       tmpDir,
+		DeleteBuildDir: true,
+	}
+
+	// Test
+	require.NoError(t, ds.cleanUpBuildDir())
+
+	// Verify
+	_, err = os.Stat(tmpDir)
+	assert.ErrorIs(t, err, fs.ErrNotExist)
+}
+
+func TestDirStructure_CleanUpBuildDirFalse(t *testing.T) {
+	// Setup
+	tmpDir, err := os.MkdirTemp("", "eib-test-")
+	require.NoError(t, err)
+	defer os.RemoveAll(tmpDir)
+
+	ds := DirStructure{
+		BuildDir:       tmpDir,
+		DeleteBuildDir: false,
+	}
+
+	// Test
+	require.NoError(t, ds.cleanUpBuildDir())
+
+	// Verify
+	_, err = os.Stat(tmpDir)
+	require.NoError(t, err)
+}
