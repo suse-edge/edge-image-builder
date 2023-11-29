b53b01def7ec10ea91c2df7650e4d0d79e53fb36
Jason Dobies <jason_dobies@yahoo.com>
2023-11-29 16:02:58 -0500

    initial implementation of user-facing audit messages

---
+++ pkg/log/audit.go
@@ -0,0 +1,52 @@
+package log
+
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+const (
+	lineLength = 40
+
+	messageSuccess = "SUCCESS"
+	messageSkipped = "SKIPPED"
+	messageFailed  = "FAILED " // leave the trailing space for consistent lengths
+)
+
+type auditStatus func(component string)
+
+// Audit displays a message to the user. This shouldn't be used for debug logging purposes; all
+// messages passed in here should be user-readable.
+func Audit(message string) {
+	fmt.Println(message)
+}
+
+func AuditComponentSuccessful(component string) {
+	message := formatComponentStatus(component, messageSuccess)
+	Audit(message)
+}
+
+func AuditComponentSkipped(component string) {
+	message := formatComponentStatus(component, messageSkipped)
+	Audit(message)
+}
+
+func AuditComponentFailed(component string) {
+	message := formatComponentStatus(component, messageFailed)
+	Audit(message)
+}
+
+func formatComponentStatus(component, status string) string {
+	// Example output:
+	// Component ... [STATUS]
+
+	name := cases.Title(language.English).String(component)
+	numDots := lineLength - (len(name) + 2 + 9)  // 2=spaces before/after dots, 9=status msg + []
+	dots := strings.Repeat(".", numDots)
+
+	message := fmt.Sprintf("%s %s [%s]", name, dots, status)
+	return message
+}
---
+++ pkg/log/audit_test.go
@@ -0,0 +1,44 @@
+package log
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFormatComponentStatus(t *testing.T) {
+	// Tests
+	tests := []struct {
+		testName  string
+		component string
+		status    string
+		expected  string
+	}{
+		{
+			testName:  "Success test",
+			component: "myComponent",
+			status:	messageSuccess,
+			expected:  "Mycomponent .................. [SUCCESS]",
+		},
+		{
+			testName:  "Skipped test",
+			component: "my component",
+			status:	messageSkipped,
+			expected:  "My Component ................. [SKIPPED]",
+		},
+		{
+			testName:  "Failed test",
+			component: "MYCOMPONENT",
+			status:	messageFailed,
+			expected:  "Mycomponent .................. [FAILED ]",
+		},
+	}
+
+	// Run
+	for _, test := range tests {
+		t.Run(test.testName, func(t *testing.T) {
+			found := formatComponentStatus(test.component, test.status)
+			assert.Equal(t, test.expected, found)
+		})
+	}
+}
