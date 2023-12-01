30e8cb5e3ecd8bccd50d40f91b716276dc3177a6
Atanas Dinov <atanas.dinov@suse.com>
2023-12-01 09:33:13 +0200

    Write logs to file instead of stdout (#50)
    * Write logs to file instead of stdout
    
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>
    
    * Drop verbose flag
    
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>
    
    ---------
    
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>

--- cmd/eib/main.go
+++ cmd/eib/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/suse-edge/edge-image-builder/pkg/build"
 	"github.com/suse-edge/edge-image-builder/pkg/image"
@@ -17,7 +19,6 @@ const (
 	argConfigDir   = "config-dir"
 	argBuildDir    = "build-dir"
 	argDeleteBuild = "delete-build-dir"
-	argVerbose     = "verbose"
 )
 
 func processArgs() (*image.Context, error) {
@@ -26,7 +27,6 @@ func processArgs() (*image.Context, error) {
 		configDir      string
 		buildDir       string
 		deleteBuildDir bool
-		verbose        bool
 	)
 
 	flag.StringVar(&configFile, argConfigFile, "", "name of the image configuration file")
@@ -34,10 +34,7 @@ func processArgs() (*image.Context, error) {
 	flag.StringVar(&buildDir, argBuildDir, "", "full path to the directory to store build artifacts")
 	flag.BoolVar(&deleteBuildDir, argDeleteBuild, false,
 		"if specified, the build directory will be deleted after the image is built")
-	flag.BoolVar(&verbose, argVerbose, false, "enables extra logging information")
 	flag.Parse()
-
-	setupLogging(verbose)
 
 	imageDefinition, err := parseImageDefinition(configFile, configDir)
 	if err != nil {
@@ -54,29 +51,29 @@ func processArgs() (*image.Context, error) {
 		return nil, fmt.Errorf("building dir structure: %w", err)
 	}
 
-	return ctx, err
+	setupLogging(ctx)
+
+	return ctx, nil
 }
 
-func setupLogging(verbose bool) {
-	logLevel := zap.InfoLevel
-	if verbose {
-		logLevel = zap.DebugLevel
-	}
+func generateBuildLogFilename(ctx *image.Context) string {
+	const buildLogFile = "eib-build-%s.log"
 
-	encoderCfg := zap.NewProductionEncoderConfig()
-	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	timestamp := time.Now().Format("Jan02_15-04-05")
+	filename := fmt.Sprintf(buildLogFile, timestamp)
 
-	logConfig := zap.Config{
-		Level:         zap.NewAtomicLevelAt(logLevel),
-		Encoding:      "console",
-		EncoderConfig: encoderCfg,
-		OutputPaths: []string{
-			"stdout",
-		},
-		ErrorOutputPaths: []string{
-			"stderr",
-		},
-	}
+	return filepath.Join(ctx.BuildDir, filename)
+}
+
+func setupLogging(ctx *image.Context) {
+	logFilename := generateBuildLogFilename(ctx)
+
+	logConfig := zap.NewProductionConfig()
+	logConfig.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	logConfig.Encoding = "console"
+	logConfig.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	logConfig.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	logConfig.OutputPaths = []string{logFilename}
 
 	logger := zap.Must(logConfig.Build())
 
@@ -113,7 +110,8 @@ func validateImageConfigDir(configDir string) error {
 func main() {
 	ctx, err := processArgs()
 	if err != nil {
-		zap.L().Fatal("CLI arguments could not be parsed", zap.Error(err))
+		// use standard logger, zap is not yet configured
+		log.Fatalf("CLI arguments could not be parsed: %s", err)
 	}
 
 	builder := build.New(ctx)
