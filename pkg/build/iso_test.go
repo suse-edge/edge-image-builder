39abb041be553a7aa40bd2702eb2758b3b500526
Jason Dobies <jason_dobies@yahoo.com>
2024-01-04 15:37:38 -0500

    Initial work towards ISO unsquash

--- pkg/build/iso_test.go
+++ pkg/build/iso_test.go
@@ -2,10 +2,8 @@ package build
 
 import (
 	"os"
-	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/suse-edge/edge-image-builder/pkg/image"
 )
@@ -63,44 +61,4 @@ func TestDeleteExistingImage(t *testing.T) {
 	_, err = os.Stat(builder.generateOutputImageFilename())
 	require.Error(t, err)
 	require.True(t, os.IsNotExist(err))
-}
-
-func TestCreateXorrisoCommand(t *testing.T) {
-	// Setup
-	builder := Builder{
-		context: &image.Context{
-			ImageConfigDir: "config-dir",
-			CombustionDir:  "combustion",
-			ImageDefinition: &image.Definition{
-				Image: image.Image{
-					BaseImage:       "base-image",
-					OutputImageName: "build-image",
-				},
-			},
-		},
-	}
-
-	// Test
-	cmd, logfile, err := builder.createXorrisoCommand()
-
-	// Verify
-	require.NoError(t, err)
-
-	defer func() {
-		assert.NoError(t, os.Remove(logfile.Name()))
-	}()
-
-	assert.Equal(t, xorrisoExec, cmd.Path)
-
-	expectedString := "/usr/bin/xorriso " +
-		"-indev config-dir/images/base-image " +
-		"-outdev config-dir/build-image " +
-		"-map combustion /combustion " +
-		"-boot_image any replay -changes_pending yes"
-	expected := strings.Split(expectedString, " ")
-	assert.Equal(t, expected, cmd.Args)
-
-	assert.NotNil(t, logfile)
-	assert.Equal(t, logfile, cmd.Stdout)
-	assert.Equal(t, logfile, cmd.Stderr)
 }
