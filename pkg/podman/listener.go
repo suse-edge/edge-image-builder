9aae0707ed93ad0c17f80e79debd34174f98aaf6
Ivo Petrov <ivo.petrov@suse.com>
2024-01-09 07:55:33 -0500

    Add EIB podman library

---
+++ pkg/podman/listener.go
@@ -0,0 +1,46 @@
+package podman
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	podmanArgsBase        = "--log-level=debug system service -t 0"
+	podmanExec            = "/usr/bin/podman"
+	podmanListenerLogFile = "podman-system-service.log"
+)
+
+// creates a listening service that answers API calls for Podman (https://docs.podman.io/en/v4.8.2/markdown/podman-system-service.1.html)
+// only way to start the service from within a container - https://github.com/containers/podman/tree/v4.8.2/pkg/bindings#starting-the-service-manually
+func setupAPIListener(out string) error {
+	cmd, logfile, err := preparePodmanCommand(out)
+	if err != nil {
+		return fmt.Errorf("configuring the podman system serice command: %w", err)
+	}
+	defer logfile.Close()
+
+	err = cmd.Start()
+	if err != nil {
+		return fmt.Errorf("error running podman system service: %w", err)
+	}
+
+	return err
+}
+
+func preparePodmanCommand(out string) (*exec.Cmd, *os.File, error) {
+	logFile, err := os.Create(filepath.Join(out, podmanListenerLogFile))
+	if err != nil {
+		return nil, nil, fmt.Errorf("generating podman lister log file: %w", err)
+	}
+
+	args := strings.Split(podmanArgsBase, " ")
+	cmd := exec.Command(podmanExec, args...)
+	cmd.Stdout = logFile
+	cmd.Stderr = logFile
+
+	return cmd, logFile, nil
+}
