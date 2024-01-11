a6b72562e41af040a0cf228e654c023d8c9a3520
Ivo Petrov <ivo.petrov@suse.com>
2024-01-11 08:59:30 -0500

    Move rpm copy logic out of the combustion package

--- pkg/combustion/rpm.go
+++ pkg/combustion/rpm.go
@@ -10,6 +10,7 @@ import (
 	"github.com/suse-edge/edge-image-builder/pkg/fileio"
 	"github.com/suse-edge/edge-image-builder/pkg/image"
 	"github.com/suse-edge/edge-image-builder/pkg/log"
+	"github.com/suse-edge/edge-image-builder/pkg/rpm"
 	"github.com/suse-edge/edge-image-builder/pkg/template"
 )
 
@@ -30,13 +31,7 @@ func configureRPMs(ctx *image.Context) ([]string, error) {
 
 	rpmSourceDir := generateComponentPath(ctx, userRPMsDir)
 
-	rpmFileNames, err := getRPMFileNames(rpmSourceDir)
-	if err != nil {
-		log.AuditComponentFailed(rpmComponentName)
-		return nil, fmt.Errorf("getting RPM file names: %w", err)
-	}
-
-	err = copyRPMs(rpmSourceDir, ctx.CombustionDir, rpmFileNames)
+	rpmFileNames, err := rpm.CopyRPMs(rpmSourceDir, ctx.CombustionDir)
 	if err != nil {
 		log.AuditComponentFailed(rpmComponentName)
 		return nil, fmt.Errorf("copying RPMs over: %w", err)
@@ -50,44 +45,6 @@ func configureRPMs(ctx *image.Context) ([]string, error) {
 
 	log.AuditComponentSuccessful(rpmComponentName)
 	return []string{script}, nil
-}
-
-func getRPMFileNames(rpmSourceDir string) ([]string, error) {
-	var rpmFileNames []string
-
-	rpms, err := os.ReadDir(rpmSourceDir)
-	if err != nil {
-		return nil, fmt.Errorf("reading RPM source dir: %w", err)
-	}
-
-	for _, rpmFile := range rpms {
-		if filepath.Ext(rpmFile.Name()) == ".rpm" {
-			rpmFileNames = append(rpmFileNames, rpmFile.Name())
-		}
-	}
-
-	if len(rpmFileNames) == 0 {
-		return nil, fmt.Errorf("no RPMs found")
-	}
-
-	return rpmFileNames, nil
-}
-
-func copyRPMs(rpmSourceDir string, rpmDestDir string, rpmFileNames []string) error {
-	if rpmDestDir == "" {
-		return fmt.Errorf("RPM destination directory cannot be empty")
-	}
-	for _, rpm := range rpmFileNames {
-		sourcePath := filepath.Join(rpmSourceDir, rpm)
-		destPath := filepath.Join(rpmDestDir, rpm)
-
-		err := fileio.CopyFile(sourcePath, destPath, fileio.NonExecutablePerms)
-		if err != nil {
-			return fmt.Errorf("copying file %s: %w", sourcePath, err)
-		}
-	}
-
-	return nil
 }
 
 func writeRPMScript(ctx *image.Context, rpmFileNames []string) (string, error) {
