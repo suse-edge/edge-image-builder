616ee2e1789295fe2b33279ce45af35a459735ec
Jason Dobies <jason.dobies@suse.com>
2024-01-19 14:40:37 -0500

    Migrated k8s validation to the new framework (#120)

---
+++ pkg/image/validation/kubernetes_test.go
@@ -0,0 +1,380 @@
+package validation
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/suse-edge/edge-image-builder/pkg/image"
+)
+
+var validNetwork = image.Network{
+	APIHost: "host.com",
+	APIVIP:  "127.0.0.1",
+}
+
+func TestValidateKubernetes(t *testing.T) {
+	tests := map[string]struct {
+		K8s                    image.Kubernetes
+		ExpectedFailedMessages []string
+	}{
+		`not defined`: {
+			K8s: image.Kubernetes{},
+		},
+		`all valid`: {
+			K8s: image.Kubernetes{
+				Network: validNetwork,
+				Nodes: []image.Node{
+					{
+						Hostname: "server",
+						Type:     image.KubernetesNodeTypeServer,
+						First:    true,
+					},
+					{
+						Hostname: "agent1",
+						Type:     image.KubernetesNodeTypeAgent,
+					},
+				},
+			},
+		},
+		`failures both sections`: {
+			K8s: image.Kubernetes{
+				Version: "1.0",
+				Network: validNetwork,
+				Nodes: []image.Node{
+					{
+						Type:  image.KubernetesNodeTypeServer,
+						First: true,
+					},
+					{
+						Hostname: "valid",
+						Type:     image.KubernetesNodeTypeAgent,
+					},
+				},
+				Manifests: image.Manifests{
+					URLs: []string{
+						"example.com",
+					},
+				},
+			},
+			ExpectedFailedMessages: []string{
+				"The 'hostname' field is required for entries in the 'nodes' section.",
+				"Entries in 'urls' must begin with either 'http://' or 'https://'.",
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctx := image.Context{
+				ImageDefinition: &image.Definition{
+					Kubernetes: test.K8s,
+				},
+			}
+			failures := validateKubernetes(&ctx)
+			assert.Len(t, failures, len(test.ExpectedFailedMessages))
+
+			var foundMessages []string
+			for _, foundValidation := range failures {
+				foundMessages = append(foundMessages, foundValidation.userMessage)
+				assert.Equal(t, k8sComponent, foundValidation.component)
+			}
+
+			for _, expectedMessage := range test.ExpectedFailedMessages {
+				assert.Contains(t, foundMessages, expectedMessage)
+			}
+
+		})
+	}
+}
+
+func TestIsKubernetesDefined(t *testing.T) {
+	result := isKubernetesDefined(&image.Kubernetes{
+		Version: "1.0",
+	})
+	assert.True(t, result)
+
+	result = isKubernetesDefined(&image.Kubernetes{
+		Network:   image.Network{},
+		Nodes:     []image.Node{},
+		Manifests: image.Manifests{},
+	})
+	assert.False(t, result)
+}
+
+func TestValidateNodes(t *testing.T) {
+	tests := map[string]struct {
+		K8s                    image.Kubernetes
+		ExpectedFailedMessages []string
+	}{
+		`valid`: {
+			K8s: image.Kubernetes{
+				Network: validNetwork,
+				Nodes: []image.Node{
+					{
+						Hostname: "agent1",
+						Type:     image.KubernetesNodeTypeAgent,
+					},
+					{
+						Hostname: "server",
+						Type:     image.KubernetesNodeTypeServer,
+						First:    true,
+					},
+				},
+			},
+		},
+		`no nodes`: {
+			K8s: image.Kubernetes{
+				Nodes: []image.Node{},
+			},
+		},
+		`with nodes - no network config`: {
+			K8s: image.Kubernetes{
+				Network: image.Network{},
+				Nodes: []image.Node{
+					{
+						Hostname: "host1",
+						Type:     image.KubernetesNodeTypeServer,
+					},
+					{
+						Hostname: "host2",
+						Type:     image.KubernetesNodeTypeAgent,
+					},
+				},
+			},
+			ExpectedFailedMessages: []string{
+				"The 'apiVIP' field is required in the 'network' section when defining entries under 'nodes'.",
+				"The 'apiHost' field is required in the 'network' section when defining entries under 'nodes'.",
+			},
+		},
+		`no hostname`: {
+			K8s: image.Kubernetes{
+				Network: validNetwork,
+				Nodes: []image.Node{
+					{
+						Hostname: "host1",
+						Type:     image.KubernetesNodeTypeServer,
+					},
+					{
+						Type: image.KubernetesNodeTypeServer,
+					},
+				},
+			},
+			ExpectedFailedMessages: []string{
+				"The 'hostname' field is required for entries in the 'nodes' section.",
+			},
+		},
+		`missing type`: {
+			K8s: image.Kubernetes{
+				Network: validNetwork,
+				Nodes: []image.Node{
+					{
+						Hostname: "host1",
+						Type:     image.KubernetesNodeTypeServer,
+					},
+					{
+						Hostname: "valid",
+					},
+				},
+			},
+			ExpectedFailedMessages: []string{
+				fmt.Sprintf("The 'type' field for entries in the 'nodes' section must be one of: %s", strings.Join(validNodeTypes, ", ")),
+			},
+		},
+		`invalid type`: {
+			K8s: image.Kubernetes{
+				Network: validNetwork,
+				Nodes: []image.Node{
+					{
+						Hostname: "valid",
+						Type:     image.KubernetesNodeTypeServer,
+					},
+					{
+						Hostname: "invalid",
+						Type:     "abnormal",
+					},
+				},
+			},
+			ExpectedFailedMessages: []string{
+				fmt.Sprintf("The 'type' field for entries in the 'nodes' section must be one of: %s", strings.Join(validNodeTypes, ", ")),
+			},
+		},
+		`first node incorrect type`: {
+			K8s: image.Kubernetes{
+				Network: validNetwork,
+				Nodes: []image.Node{
+					{
+						Hostname: "valid",
+						Type:     image.KubernetesNodeTypeServer,
+					},
+					{
+						Hostname: "invalid",
+						First:    true,
+						Type:     image.KubernetesNodeTypeAgent,
+					},
+				},
+			},
+			ExpectedFailedMessages: []string{
+				fmt.Sprintf("The node labeled with 'firstNode' must be of type '%s'.", image.KubernetesNodeTypeServer),
+			},
+		},
+		`duplicate entries`: {
+			K8s: image.Kubernetes{
+				Network: validNetwork,
+				Nodes: []image.Node{
+					{
+						Hostname: "foo",
+						Type:     image.KubernetesNodeTypeServer,
+						First:    true,
+					},
+					{
+						Hostname: "bar",
+						Type:     image.KubernetesNodeTypeAgent,
+					},
+					{
+						Hostname: "bar",
+						Type:     image.KubernetesNodeTypeAgent,
+					},
+					{
+						Hostname: "foo",
+						Type:     image.KubernetesNodeTypeAgent,
+					},
+				},
+			},
+			ExpectedFailedMessages: []string{
+				"The 'nodes' section contains duplicate entries: bar, foo",
+			},
+		},
+		`no server node`: {
+			K8s: image.Kubernetes{
+				Network: validNetwork,
+				Nodes: []image.Node{
+					{
+						Hostname: "foo",
+						Type:     image.KubernetesNodeTypeAgent,
+					},
+					{
+						Hostname: "bar",
+						Type:     image.KubernetesNodeTypeAgent,
+					},
+				},
+			},
+			ExpectedFailedMessages: []string{
+				fmt.Sprintf("There must be at least one node of type '%s' defined.", image.KubernetesNodeTypeServer),
+			},
+		},
+		`multiple first nodes`: {
+			K8s: image.Kubernetes{
+				Network: validNetwork,
+				Nodes: []image.Node{
+					{
+						Hostname: "foo",
+						Type:     image.KubernetesNodeTypeServer,
+						First:    true,
+					},
+					{
+						Hostname: "bar",
+						Type:     image.KubernetesNodeTypeServer,
+						First:    true,
+					},
+				},
+			},
+			ExpectedFailedMessages: []string{
+				"Only one node may be specified as the cluster initializer (by including the 'firstNode' field).",
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			k := test.K8s
+			failures := validateNodes(&k)
+			assert.Len(t, failures, len(test.ExpectedFailedMessages))
+
+			var foundMessages []string
+			for _, foundValidation := range failures {
+				foundMessages = append(foundMessages, foundValidation.userMessage)
+				assert.Equal(t, k8sComponent, foundValidation.component)
+			}
+
+			for _, expectedMessage := range test.ExpectedFailedMessages {
+				assert.Contains(t, foundMessages, expectedMessage)
+			}
+
+		})
+	}
+}
+
+func TestValidateManifestURLs(t *testing.T) {
+	tests := map[string]struct {
+		K8s                    image.Kubernetes
+		ExpectedFailedMessages []string
+	}{
+		`valid`: {
+			K8s: image.Kubernetes{
+				Manifests: image.Manifests{
+					URLs: []string{
+						"http://valid1.com",
+						"https://valid2.com",
+					},
+				},
+			},
+		},
+		`no URLs`: {
+			K8s: image.Kubernetes{
+				Manifests: image.Manifests{},
+			},
+		},
+		`invalid prefix`: {
+			K8s: image.Kubernetes{
+				Manifests: image.Manifests{
+					URLs: []string{
+						"http://valid.com",
+						"https://also-valid.com",
+						"invalid.com",
+						"nope.com",
+					},
+				},
+			},
+			ExpectedFailedMessages: []string{
+				"Entries in 'urls' must begin with either 'http://' or 'https://'.",
+				"Entries in 'urls' must begin with either 'http://' or 'https://'.",
+			},
+		},
+		`duplicate URLs`: {
+			K8s: image.Kubernetes{
+				Manifests: image.Manifests{
+					URLs: []string{
+						"http://foo.com",
+						"http://bar.com",
+						"http://foo.com",
+						"http://bar.com",
+					},
+				},
+			},
+			ExpectedFailedMessages: []string{
+				"The 'urls' field contains duplicate entries: http://foo.com",
+				"The 'urls' field contains duplicate entries: http://bar.com",
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			k := test.K8s
+			failures := validateManifestURLs(&k)
+			assert.Len(t, failures, len(test.ExpectedFailedMessages))
+
+			var foundMessages []string
+			for _, foundValidation := range failures {
+				foundMessages = append(foundMessages, foundValidation.userMessage)
+				assert.Equal(t, k8sComponent, foundValidation.component)
+			}
+
+			for _, expectedMessage := range test.ExpectedFailedMessages {
+				assert.Contains(t, foundMessages, expectedMessage)
+			}
+		})
+	}
+}
