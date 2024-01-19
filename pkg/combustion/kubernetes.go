46e8e55b8829aa25345e344b5534fecb9b2ebc6e
Atanas Dinov <atanas.dinov@suse.com>
2024-01-19 18:23:31 +0200

    Implement multi node RKE2 architecture
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>

--- pkg/combustion/kubernetes.go
+++ pkg/combustion/kubernetes.go
@@ -22,15 +22,25 @@ const (
 	k8sDir              = "kubernetes"
 	k8sConfigDir        = "config"
 	k8sServerConfigFile = "server.yaml"
+	k8sAgentConfigFile  = "agent.yaml"
 	rke2InstallScript   = "15-rke2-install.sh"
 
+	tokenKey        = "token"
 	cniKey          = "cni"
 	cniDefaultValue = image.CNITypeCilium
+	serverKey       = "server"
+	tlsSANKey       = "tls-san"
 )
 
 var (
 	//go:embed templates/15-rke2-single-node-installer.sh.tpl
 	rke2SingleNodeInstaller string
+
+	//go:embed templates/15-rke2-multi-node-installer.sh.tpl
+	rke2MultiNodeInstaller string
+
+	//go:embed templates/rke2-ha-api.yaml.tpl
+	rke2HAAPIManifest string
 )
 
 func configureKubernetes(ctx *image.Context) ([]string, error) {
@@ -88,45 +98,123 @@ func configureRKE2(ctx *image.Context) (string, error) {
 		return "", fmt.Errorf("copying RKE2 installer script: %w", err)
 	}
 
-	config, err := parseKubernetesConfig(ctx)
-	if err != nil {
-		return "", fmt.Errorf("parsing RKE2 config: %w", err)
+	if len(ctx.ImageDefinition.Kubernetes.Nodes) > 1 {
+		zap.S().Info("Configuring multi node RKE2 cluster...")
+		return configureMultiNodeRKE2(ctx)
 	}
 
-	cni, multusEnabled, err := extractCNI(config)
+	zap.S().Info("Configuring single node RKE2 cluster...")
+	return configureSingleNodeRKE2(ctx)
+}
+
+func configureSingleNodeRKE2(ctx *image.Context) (string, error) {
+	serverConfig, err := parseKubernetesConfig(ctx, k8sServerConfigFile)
 	if err != nil {
-		return "", fmt.Errorf("extracting CNI from RKE2 config: %w", err)
+		return "", fmt.Errorf("parsing RKE2 server config: %w", err)
 	}
 
-	configFile, err := storeKubernetesConfig(ctx, config, image.KubernetesDistroRKE2)
-	if err != nil {
-		return "", fmt.Errorf("storing RKE2 config file: %w", err)
+	// Establish sane default values
+	setClusterCNI(serverConfig)
+	delete(serverConfig, tlsSANKey) // TODO: Figure out whether tls-san is fine to keep
+	delete(serverConfig, serverKey)
+
+	if err = storeKubernetesConfig(ctx, serverConfig, k8sServerConfigFile); err != nil {
+		return "", fmt.Errorf("storing RKE2 server config file: %w", err)
 	}
 
-	installPath, imagesPath, err := ctx.KubernetesArtefactDownloader.DownloadArtefacts(
-		ctx.ImageDefinition.Image.Arch,
-		ctx.ImageDefinition.Kubernetes.Version,
-		cni,
-		multusEnabled,
-		ctx.CombustionDir,
-	)
+	installPath, imagesPath, err := downloadRKE2Artefacts(ctx, serverConfig)
 	if err != nil {
 		return "", fmt.Errorf("downloading RKE2 artefacts: %w", err)
 	}
 
 	rke2 := struct {
-		image.Kubernetes
 		ConfigFile  string
 		InstallPath string
 		ImagesPath  string
 	}{
-		Kubernetes:  ctx.ImageDefinition.Kubernetes,
-		ConfigFile:  configFile,
+		ConfigFile:  k8sServerConfigFile,
 		InstallPath: installPath,
 		ImagesPath:  imagesPath,
 	}
 
-	data, err := template.Parse(rke2InstallScript, rke2SingleNodeInstaller, &rke2)
+	return storeRKE2Installer(ctx, "single-node-rke2", rke2SingleNodeInstaller, &rke2)
+}
+
+func configureMultiNodeRKE2(ctx *image.Context) (string, error) {
+	firstNode := findKubernetesInitialiserNode(&ctx.ImageDefinition.Kubernetes)
+	if firstNode == "" {
+		return "", fmt.Errorf("failed to determine first node in cluster")
+	}
+
+	serverConfig, err := parseKubernetesConfig(ctx, k8sServerConfigFile)
+	if err != nil {
+		return "", fmt.Errorf("parsing RKE2 server config: %w", err)
+	}
+
+	// Establish sane default values
+	setClusterCNI(serverConfig)
+	setClusterToken(serverConfig)
+	setClusterAPIHost(serverConfig, ctx.ImageDefinition.Kubernetes.Network.APIHost)
+	setClusterAPIAddress(serverConfig, ctx.ImageDefinition.Kubernetes.Network.APIVIP)
+
+	if err = storeKubernetesConfig(ctx, serverConfig, k8sServerConfigFile); err != nil {
+		return "", fmt.Errorf("storing RKE2 server config file: %w", err)
+	}
+
+	agentConfig, err := parseKubernetesConfig(ctx, k8sAgentConfigFile)
+	if err != nil {
+		return "", fmt.Errorf("parsing RKE2 agent config: %w", err)
+	}
+
+	// Ensure the agent uses the same cluster configuration values as the server
+	agentConfig[tokenKey] = serverConfig[tokenKey]
+	agentConfig[cniKey] = serverConfig[cniKey]
+	agentConfig[serverKey] = serverConfig[serverKey]
+	agentConfig[tlsSANKey] = serverConfig[tlsSANKey]
+
+	if err = storeKubernetesConfig(ctx, agentConfig, k8sAgentConfigFile); err != nil {
+		return "", fmt.Errorf("storing RKE2 agent config file: %w", err)
+	}
+
+	// Drop values not applicable to the initialiser
+	delete(serverConfig, tlsSANKey)
+	delete(serverConfig, serverKey)
+
+	serverConfigFile := fmt.Sprintf("first_%s", k8sServerConfigFile)
+
+	if err = storeKubernetesConfig(ctx, serverConfig, serverConfigFile); err != nil {
+		return "", fmt.Errorf("storing RKE2 initialising server config file: %w", err)
+	}
+
+	installPath, imagesPath, err := downloadRKE2Artefacts(ctx, serverConfig)
+	if err != nil {
+		return "", fmt.Errorf("downloading RKE2 artefacts: %w", err)
+	}
+
+	haManifest, err := storeHighAvailabilityRKE2Manifest(ctx)
+	if err != nil {
+		return "", fmt.Errorf("storing RKE2 HA API manifest: %w", err)
+	}
+
+	rke2 := struct {
+		image.Kubernetes
+		FirstNode   string
+		HAManifest  string
+		InstallPath string
+		ImagesPath  string
+	}{
+		Kubernetes:  ctx.ImageDefinition.Kubernetes,
+		FirstNode:   firstNode,
+		HAManifest:  haManifest,
+		InstallPath: installPath,
+		ImagesPath:  imagesPath,
+	}
+
+	return storeRKE2Installer(ctx, "multi-node-rke2", rke2MultiNodeInstaller, &rke2)
+}
+
+func storeRKE2Installer(ctx *image.Context, templateName, templateContents string, templateValues any) (string, error) {
+	data, err := template.Parse(templateName, templateContents, templateValues)
 	if err != nil {
 		return "", fmt.Errorf("parsing RKE2 install template: %w", err)
 	}
@@ -139,42 +227,142 @@ func configureRKE2(ctx *image.Context) (string, error) {
 	return rke2InstallScript, nil
 }
 
-func parseKubernetesConfig(ctx *image.Context) (map[string]any, error) {
-	auditDefaultCNI := func() {
-		auditMessage := fmt.Sprintf("Kubernetes CNI not explicitly set, defaulting to: %s", cniDefaultValue)
-		log.Audit(auditMessage)
+func downloadRKE2Artefacts(ctx *image.Context, clusterConfig map[string]any) (installPath, imagesPath string, err error) {
+	cni, multusEnabled, err := extractCNI(clusterConfig)
+	if err != nil {
+		return "", "", fmt.Errorf("extracting CNI from cluster config: %w", err)
 	}
 
+	return ctx.KubernetesArtefactDownloader.DownloadArtefacts(
+		ctx.ImageDefinition.Image.Arch,
+		ctx.ImageDefinition.Kubernetes.Version,
+		cni,
+		multusEnabled,
+		ctx.CombustionDir,
+	)
+}
+
+func findKubernetesInitialiserNode(kubernetes *image.Kubernetes) string {
+	for _, node := range kubernetes.Nodes {
+		if node.First {
+			return node.Hostname
+		}
+	}
+
+	// Use the first server node as an initialiser
+	for _, node := range kubernetes.Nodes {
+		if node.Type == image.KubernetesNodeTypeServer {
+			zap.S().Infof("Using '%s' for cluster initialiser since none of the nodes was explicitly configured", node.Hostname)
+			return node.Hostname
+		}
+	}
+
+	return ""
+}
+
+func storeHighAvailabilityRKE2Manifest(ctx *image.Context) (string, error) {
+	const haManifest = "rke2-ha-api.yaml"
+
+	manifest := struct {
+		APIAddress string
+	}{
+		APIAddress: ctx.ImageDefinition.Kubernetes.Network.APIVIP,
+	}
+
+	data, err := template.Parse("rke2-ha-api", rke2HAAPIManifest, &manifest)
+	if err != nil {
+		return "", fmt.Errorf("parsing RKE2 HA API template: %w", err)
+	}
+
+	installScript := filepath.Join(ctx.CombustionDir, haManifest)
+	if err = os.WriteFile(installScript, []byte(data), fileio.ExecutablePerms); err != nil {
+		return "", fmt.Errorf("writing RKE2 HA API manifest: %w", err)
+	}
+
+	return haManifest, nil
+}
+
+func parseKubernetesConfig(ctx *image.Context, configFile string) (map[string]any, error) {
 	config := map[string]any{}
 
 	configDir := generateComponentPath(ctx, k8sDir)
-	configFile := filepath.Join(configDir, k8sConfigDir, k8sServerConfigFile)
+	file := filepath.Join(configDir, k8sConfigDir, configFile)
 
-	b, err := os.ReadFile(configFile)
+	b, err := os.ReadFile(file)
 	if err != nil {
 		if !errors.Is(err, fs.ErrNotExist) {
-			return nil, fmt.Errorf("reading kubernetes config file: %w", err)
+			return nil, fmt.Errorf("reading kubernetes config file '%s': %w", configFile, err)
 		}
 
-		auditDefaultCNI()
-		zap.S().Infof("Kubernetes server config file not provided, proceeding with CNI: %s", cniDefaultValue)
-
-		config[cniKey] = cniDefaultValue
+		// Use an empty config which will be automatically populated later
 		return config, nil
 	}
 
 	if err = yaml.Unmarshal(b, &config); err != nil {
-		return nil, fmt.Errorf("parsing kubernetes config file: %w", err)
-	}
-
-	if _, ok := config[cniKey]; !ok {
-		auditDefaultCNI()
-		zap.S().Infof("CNI not set in config file, proceeding with CNI: %s", cniDefaultValue)
-
-		config[cniKey] = cniDefaultValue
+		return nil, fmt.Errorf("parsing kubernetes config file '%s': %w", configFile, err)
 	}
 
 	return config, nil
+}
+
+func setClusterToken(config map[string]any) {
+	if _, ok := config[tokenKey].(string); ok {
+		return
+	}
+
+	token := "foobar" // TODO: generate
+
+	zap.S().Infof("Generated cluster token: %s", token)
+	config[tokenKey] = token
+}
+
+func setClusterCNI(config map[string]any) {
+	if _, ok := config[cniKey]; ok {
+		return
+	}
+
+	auditMessage := fmt.Sprintf("Kubernetes CNI not explicitly set, defaulting to: %s", cniDefaultValue)
+	log.Audit(auditMessage)
+
+	zap.S().Infof("CNI not set in config file, proceeding with CNI: %s", cniDefaultValue)
+
+	config[cniKey] = cniDefaultValue
+}
+
+func setClusterAPIHost(config map[string]any, apiHost string) {
+	if apiHost == "" {
+		zap.S().Warnf("Attempted to set an empty cluster API host")
+		return
+	}
+
+	tlsSAN, ok := config[tlsSANKey]
+	if !ok {
+		config[tlsSANKey] = []string{apiHost}
+		return
+	}
+
+	switch v := tlsSAN.(type) {
+	case string:
+		config[tlsSANKey] = []string{v, apiHost}
+	case []string:
+		v = append(v, apiHost)
+		config[tlsSANKey] = v
+	case []any:
+		v = append(v, apiHost)
+		config[tlsSANKey] = v
+	default:
+		zap.S().Warnf("Ignoring invalid 'tls-san' value: %s", v)
+		config[tlsSANKey] = []string{apiHost}
+	}
+}
+
+func setClusterAPIAddress(config map[string]any, apiAddress string) {
+	if apiAddress == "" {
+		zap.S().Warnf("Attempted to set an empty cluster API address")
+		return
+	}
+
+	config[serverKey] = fmt.Sprintf("https://%s:9345", apiAddress)
 }
 
 func extractCNI(config map[string]any) (cni string, multusEnabled bool, err error) {
@@ -234,18 +422,17 @@ func parseCNIs(cnis []string) (cni string, multusEnabled bool, err error) {
 	return cni, multusEnabled, nil
 }
 
-func storeKubernetesConfig(ctx *image.Context, config map[string]any, distribution string) (string, error) {
+func storeKubernetesConfig(ctx *image.Context, config map[string]any, filename string) error {
 	data, err := yaml.Marshal(config)
 	if err != nil {
-		return "", fmt.Errorf("serializing kubernetes config: %w", err)
+		return fmt.Errorf("serializing kubernetes config: %w", err)
 	}
 
-	configFile := fmt.Sprintf("%s_config.yaml", distribution)
-	configPath := filepath.Join(ctx.CombustionDir, configFile)
+	configPath := filepath.Join(ctx.CombustionDir, filename)
 
 	if err = os.WriteFile(configPath, data, fileio.NonExecutablePerms); err != nil {
-		return "", fmt.Errorf("storing kubernetes config file: %w", err)
+		return fmt.Errorf("storing kubernetes config file: %w", err)
 	}
 
-	return configFile, nil
+	return nil
 }
--- pkg/combustion/kubernetes_test.go
+++ pkg/combustion/kubernetes_test.go
@@ -127,7 +127,7 @@ func TestConfigureKubernetes_ArtefactDownloaderErrorRKE2(t *testing.T) {
 	assert.Nil(t, scripts)
 }
 
-func TestConfigureKubernetes_SuccessfulRKE2Server(t *testing.T) {
+func TestConfigureKubernetes_SuccessfulSingleNodeRKE2Cluster(t *testing.T) {
 	ctx, teardown := setupContext(t)
 	defer teardown()
 
@@ -161,15 +161,13 @@ func TestConfigureKubernetes_SuccessfulRKE2Server(t *testing.T) {
 	require.NoError(t, err)
 
 	contents := string(b)
-	assert.NotContains(t, contents, "export INSTALL_RKE2_TYPE=server",
-		"INSTALL_RKE2_TYPE is set when the definition file does not explicitly set it")
 	assert.Contains(t, contents, "cp server-images/* /var/lib/rancher/rke2/agent/images/")
-	assert.Contains(t, contents, "cp rke2_config.yaml /etc/rancher/rke2/config.yaml")
+	assert.Contains(t, contents, "cp server.yaml /etc/rancher/rke2/config.yaml")
 	assert.Contains(t, contents, "export INSTALL_RKE2_ARTIFACT_PATH=server-installer")
 	assert.Contains(t, contents, "systemctl enable rke2-server.service")
 
 	// Config file assertions
-	configPath := filepath.Join(ctx.CombustionDir, "rke2_config.yaml")
+	configPath := filepath.Join(ctx.CombustionDir, "server.yaml")
 
 	info, err = os.Stat(configPath)
 	require.NoError(t, err)
@@ -184,6 +182,126 @@ func TestConfigureKubernetes_SuccessfulRKE2Server(t *testing.T) {
 
 	require.Contains(t, configContents, "cni")
 	assert.Equal(t, "cilium", configContents["cni"], "default CNI is not set")
+}
+
+func TestConfigureKubernetes_SuccessfulMultiNodeRKE2Cluster(t *testing.T) {
+	ctx, teardown := setupContext(t)
+	defer teardown()
+
+	ctx.ImageDefinition.Kubernetes = image.Kubernetes{
+		Version: "v1.29.0+rke2r1",
+		Network: image.Network{
+			APIHost: "api.cluster01.hosted.on.edge.suse.com",
+			APIVIP:  "192.168.122.100",
+		},
+		Nodes: []image.Node{
+			{
+				Hostname: "node1.suse.com",
+				Type:     "server",
+			},
+			{
+				Hostname: "node2.suse.com",
+				Type:     "agent",
+			},
+		},
+	}
+	ctx.KubernetesScriptInstaller = mockKubernetesScriptInstaller{
+		installScript: func(distribution, sourcePath, destPath string) error {
+			return nil
+		},
+	}
+	ctx.KubernetesArtefactDownloader = mockKubernetesArtefactDownloader{
+		downloadArtefacts: func(arch image.Arch, version, cni string, multusEnabled bool, destPath string) (string, string, error) {
+			return "server-installer", "server-images", nil
+		},
+	}
+
+	serverConfig := map[string]any{
+		"token": "123",
+		"cni":   "canal",
+		"tls-san": []string{
+			"192.168.122.100.sslip.io",
+		},
+	}
+
+	b, err := yaml.Marshal(serverConfig)
+	require.NoError(t, err)
+
+	configDir := filepath.Join(ctx.ImageConfigDir, k8sDir, k8sConfigDir)
+	require.NoError(t, os.MkdirAll(configDir, os.ModePerm))
+	require.NoError(t, os.WriteFile(filepath.Join(configDir, "server.yaml"), b, os.ModePerm))
+
+	scripts, err := configureKubernetes(ctx)
+	require.NoError(t, err)
+	require.Len(t, scripts, 1)
+
+	// Script file assertions
+	scriptPath := filepath.Join(ctx.CombustionDir, scripts[0])
+
+	info, err := os.Stat(scriptPath)
+	require.NoError(t, err)
+
+	assert.Equal(t, fileio.ExecutablePerms, info.Mode())
+
+	b, err = os.ReadFile(scriptPath)
+	require.NoError(t, err)
+
+	contents := string(b)
+	assert.Contains(t, contents, "hosts[node1.suse.com]=server")
+	assert.Contains(t, contents, "hosts[node2.suse.com]=agent")
+	assert.Contains(t, contents, "cp server-images/* /var/lib/rancher/rke2/agent/images/")
+	assert.Contains(t, contents, "cp $CONFIGFILE /etc/rancher/rke2/config.yaml")
+	assert.Contains(t, contents, "if [ \"$HOSTNAME\" = node1.suse.com ]; then")
+	assert.Contains(t, contents, "cp rke2-ha-api.yaml /var/lib/rancher/rke2/server/manifests/rke2-ha-api.yaml")
+	assert.Contains(t, contents, "export INSTALL_RKE2_ARTIFACT_PATH=server-installer")
+	assert.Contains(t, contents, "systemctl enable rke2-$NODETYPE.service")
+
+	// Server config file assertions
+	configPath := filepath.Join(ctx.CombustionDir, "server.yaml")
+
+	info, err = os.Stat(configPath)
+	require.NoError(t, err)
+
+	assert.Equal(t, fileio.NonExecutablePerms, info.Mode())
+
+	b, err = os.ReadFile(configPath)
+	require.NoError(t, err)
+
+	var configContents map[string]any
+	require.NoError(t, yaml.Unmarshal(b, &configContents))
+
+	assert.Equal(t, "canal", configContents["cni"])
+	assert.Equal(t, "123", configContents["token"])
+	assert.Equal(t, "https://192.168.122.100:9345", configContents["server"])
+	assert.Equal(t, []any{"192.168.122.100.sslip.io", "api.cluster01.hosted.on.edge.suse.com"}, configContents["tls-san"])
+
+	// Initialising server config file assertions
+	configPath = filepath.Join(ctx.CombustionDir, "first_server.yaml")
+
+	b, err = os.ReadFile(configPath)
+	require.NoError(t, err)
+
+	configContents = map[string]any{} // clear the map
+	require.NoError(t, yaml.Unmarshal(b, configContents))
+
+	assert.Equal(t, "canal", configContents["cni"])
+	assert.Equal(t, "123", configContents["token"])
+	assert.Equal(t, nil, configContents["server"])
+	assert.Equal(t, nil, configContents["tls-san"])
+
+	// Agent config file assertions
+	configPath = filepath.Join(ctx.CombustionDir, "agent.yaml")
+
+	b, err = os.ReadFile(configPath)
+	require.NoError(t, err)
+
+	configContents = map[string]any{} // clear the map
+	require.NoError(t, yaml.Unmarshal(b, configContents))
+
+	assert.Equal(t, "canal", configContents["cni"])
+	assert.Equal(t, "123", configContents["token"])
+	assert.Equal(t, "https://192.168.122.100:9345", configContents["server"])
+	assert.Equal(t, []any{"192.168.122.100.sslip.io", "api.cluster01.hosted.on.edge.suse.com"}, configContents["tls-san"])
 }
 
 func TestExtractCNI(t *testing.T) {
@@ -301,4 +419,147 @@ func TestExtractCNI(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestFindKubernetesInitialiserNode(t *testing.T) {
+	tests := []struct {
+		name         string
+		nodes        []image.Node
+		expectedNode string
+	}{
+		{
+			name:         "Empty list of nodes",
+			expectedNode: "",
+		},
+		{
+			name: "Agent list",
+			nodes: []image.Node{
+				{
+					Hostname: "host1",
+					Type:     "agent",
+				},
+				{
+					Hostname: "host2",
+					Type:     "agent",
+				},
+			},
+
+			expectedNode: "",
+		},
+		{
+			name: "Server node labeled as first",
+			nodes: []image.Node{
+				{
+					Hostname: "host1",
+					Type:     "agent",
+				},
+				{
+					Hostname: "host2",
+					Type:     "server",
+				},
+				{
+					Hostname: "host3",
+					Type:     "server",
+					First:    true,
+				},
+			},
+			expectedNode: "host3",
+		},
+		{
+			name: "Server node first in list",
+			nodes: []image.Node{
+				{
+					Hostname: "host1",
+					Type:     "agent",
+				},
+				{
+					Hostname: "host2",
+					Type:     "server",
+				},
+				{
+					Hostname: "host3",
+					Type:     "server",
+				},
+			},
+			expectedNode: "host2",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			kubernetes := &image.Kubernetes{Nodes: test.nodes}
+			assert.Equal(t, test.expectedNode, findKubernetesInitialiserNode(kubernetes))
+		})
+	}
+}
+
+func TestSetClusterAPIHost(t *testing.T) {
+	tests := []struct {
+		name           string
+		config         map[string]any
+		apiHost        string
+		expectedTLSSAN any
+	}{
+		{
+			name:           "Empty TLS SAN",
+			config:         map[string]any{},
+			apiHost:        "",
+			expectedTLSSAN: nil,
+		},
+		{
+			name:           "Missing TLS SAN",
+			config:         map[string]any{},
+			apiHost:        "api.cluster01.hosted.on.edge.suse.com",
+			expectedTLSSAN: []string{"api.cluster01.hosted.on.edge.suse.com"},
+		},
+		{
+			name: "Invalid TLS SAN",
+			config: map[string]any{
+				"tls-san": 5,
+			},
+			apiHost:        "api.cluster01.hosted.on.edge.suse.com",
+			expectedTLSSAN: []string{"api.cluster01.hosted.on.edge.suse.com"},
+		},
+		{
+			name: "Existing TLS SAN string",
+			config: map[string]any{
+				"tls-san": "random",
+			},
+			apiHost:        "api.cluster01.hosted.on.edge.suse.com",
+			expectedTLSSAN: []string{"random", "api.cluster01.hosted.on.edge.suse.com"},
+		},
+		{
+			name: "Existing TLS SAN string list",
+			config: map[string]any{
+				"tls-san": []string{"random"},
+			},
+			apiHost:        "api.cluster01.hosted.on.edge.suse.com",
+			expectedTLSSAN: []string{"random", "api.cluster01.hosted.on.edge.suse.com"},
+		},
+		{
+			name: "Existing TLS SAN list",
+			config: map[string]any{
+				"tls-san": []any{"random"},
+			},
+			apiHost:        "api.cluster01.hosted.on.edge.suse.com",
+			expectedTLSSAN: []any{"random", "api.cluster01.hosted.on.edge.suse.com"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			setClusterAPIHost(test.config, test.apiHost)
+			assert.Equal(t, test.expectedTLSSAN, test.config["tls-san"])
+		})
+	}
+}
+
+func TestSetClusterAPIAddress(t *testing.T) {
+	config := map[string]any{}
+
+	setClusterAPIAddress(config, "")
+	assert.NotContains(t, config, "server")
+
+	setClusterAPIAddress(config, "192.168.122.50")
+	assert.Equal(t, "https://192.168.122.50:9345", config["server"])
 }
