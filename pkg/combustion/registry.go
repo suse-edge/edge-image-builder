67c297e28ed56b8945eccac8d8660becdc135f10
dbw7 <danial.bekhit@suse.com>
2024-02-10 20:15:24 -0500

    rename template

--- pkg/combustion/registry.go
+++ pkg/combustion/registry.go
@@ -45,7 +45,7 @@ const (
 //go:embed templates/hauler-manifest.yaml.tpl
 var haulerManifest string
 
-//go:embed templates/14-embedded-registry.sh.tpl
+//go:embed templates/26-embedded-registry.sh.tpl
 var registryScript string
 
 //go:embed templates/registries.yaml.tpl
