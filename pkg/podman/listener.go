19e57a68aef002fa4cc16d69e245649804bd938e
Ivo Petrov <ivo.petrov@suse.com>
2024-03-14 08:21:01 -0400

    Add a wait mechanism for the api listener

--- pkg/podman/listener.go
+++ pkg/podman/listener.go
@@ -7,17 +7,25 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
+
+	"github.com/suse-edge/edge-image-builder/pkg/log"
+	"go.uber.org/zap"
 )
 
 const (
 	podmanArgsBase        = "--log-level=debug system service -t 0"
 	podmanExec            = "/usr/bin/podman"
 	podmanListenerLogFile = "podman-system-service.log"
+	podmanSocketPath      = "/run/podman/podman.sock"
 )
 
 // creates a listening service that answers API calls for Podman (https://docs.podman.io/en/v4.8.2/markdown/podman-system-service.1.html)
 // only way to start the service from within a container - https://github.com/containers/podman/tree/v4.8.2/pkg/bindings#starting-the-service-manually
 func setupAPIListener(out string) error {
+	log.Audit("Setting up Podman API listener...")
+	zap.L().Info("Setting up Podman API listener...")
+
 	logFile, err := os.Create(filepath.Join(out, podmanListenerLogFile))
 	if err != nil {
 		return fmt.Errorf("creating podman listener log file: %w", err)
@@ -31,7 +39,7 @@ func setupAPIListener(out string) error {
 		return fmt.Errorf("error running podman system service: %w", err)
 	}
 
-	return nil
+	return waitForPodmanSock()
 }
 
 func preparePodmanCommand(out io.Writer) *exec.Cmd {
@@ -41,4 +49,24 @@ func preparePodmanCommand(out io.Writer) *exec.Cmd {
 	cmd.Stderr = out
 
 	return cmd
+}
+
+func waitForPodmanSock() error {
+	const (
+		retries      = 5
+		sleepSeconds = 3
+	)
+
+	zap.S().Infof("Waiting for '%s' to be created", podmanSocketPath)
+	for i := 0; i < retries; i++ {
+		if _, err := os.Stat(podmanSocketPath); err == nil {
+			zap.S().Infof("'%s' file has been created successfully", podmanSocketPath)
+			return nil
+		}
+
+		zap.S().Infof("'%s' file is not yet created, retrying in %d seconds", podmanSocketPath, sleepSeconds)
+		time.Sleep(sleepSeconds * time.Second)
+	}
+
+	return fmt.Errorf("'%s' file was not created in the expected time", podmanSocketPath)
 }
