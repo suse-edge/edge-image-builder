2b98ee3f83217ef4d03e2fb6baad10c1cdca1def
e-minguez <e.minguez@gmail.com>
2024-03-25 13:18:28 +0100

    Removed k3s stuff

--- examples/edge-stack-iso/custom/files/basic-setup.sh
+++ examples/edge-stack-iso/custom/files/basic-setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Pre-requisites. Cluster already running
-export RKE2KUBECTL="/var/lib/rancher/rke2/bin/kubectl"
-export K3SKUBECTL="/opt/bin/kubectl"
-export RKE2KUBECONFIG="/etc/rancher/rke2/rke2.yaml"
-export K3SKUBECONFIG="/etc/rancher/k3s/k3s.yaml"
+export KUBECTL="/var/lib/rancher/rke2/bin/kubectl"
+export KUBECONFIG="/etc/rancher/rke2/rke2.yaml"
 
 ##################
 # METAL3 DETAILS #
@@ -34,32 +32,3 @@ die(){
   echo ${1} 1>&2
   exit ${2}
 }
-
-setup_kubetools(){
-  RETRIES=10
-  SLEEPTIME=2
-
-  # Identify if K3s or RKE2 (timeout = reties * sleep time)
-  t=${RETRIES}
-  until [ -e ${RKE2KUBECONFIG} ] || [ -e ${K3SKUBECONFIG} ] && (( t-- > 0 )); do
-    sleep ${SLEEPTIME}
-  done
-  if [ -e "${RKE2KUBECONFIG}" ]; then
-    export KUBECONFIG=${RKE2KUBECONFIG}
-    export KUBECTL=${RKE2KUBECTL}
-  else
-    export KUBECONFIG=${K3SKUBECONFIG}
-    export KUBECTL=${K3SKUBECTL}
-  fi
-
-  # Wait for the node to be available, meaning the K8s API is available
-  while ! ${KUBECTL} wait --for condition=ready node $(cat /etc/hostname | tr '[:upper:]' '[:lower:]') ; do sleep 2 ; done
-
-  # https://github.com/rancher/rke2/issues/3958
-  if [ "${KUBECTL}" == "${RKE2KUBECTL}" ]; then
-    # Wait for the rke2-ingress-nginx-controller DS to be available if using RKE2
-    while ! ${KUBECTL} rollout status daemonset -n kube-system rke2-ingress-nginx-controller --timeout=60s; do sleep 2 ; done
-  fi
-}
-
-setup_kubetools
