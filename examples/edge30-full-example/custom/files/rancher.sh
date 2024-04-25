85eeea82231e27899fc4f10a90c3fee0f69bea78
e-minguez <e.minguez@gmail.com>
2024-04-25 15:21:03 +0200

    Elemental 1.4.3 and CM issues

--- examples/edge30-full-example/custom/files/rancher.sh
+++ examples/edge30-full-example/custom/files/rancher.sh
@@ -17,7 +17,7 @@ trap 'catch $? $LINENO' EXIT
 catch() {
   if [ "$1" != "0" ]; then
     echo "Error $1 occurred on $2"
-    ${KUBECTL} delete configmap ${METAL3LOCKCMNAME} -n ${METAL3LOCKNAMESPACE}
+    ${KUBECTL} delete configmap ${RANCHERLOCKCMNAME} -n ${RANCHERLOCKNAMESPACE}
   fi
 }
 
